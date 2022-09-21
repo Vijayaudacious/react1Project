b7364d040525e93a6db547949a1d881da610e4da
vijayaudacious <vijayaudacious777@gmail.com>
2022-09-21 11:40:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  let name = "Vijay";
+  return (        
+    <>
+       
+    
+       <Navbar title=" 09 Blog " AboutText="About US" />
+       {/* <Navbar/> */}
+    </>
+  );
+}
+
+export default App;
